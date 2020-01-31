a71971520239772fc5fe4a92ddaf4467e553a010
Tommy Yates <7tjyates7@gmail.com>
2020-01-31 14:31:29 -0500

    all 13 correct

--- content/challenges/hard/2 - Recurse Palindrome/func.js
+++ content/challenges/hard/2 - Recurse Palindrome/func.js
@@ -4,4 +4,24 @@
 module.exports = (str) => {
   // put your code here
   // don't touch anything else
+  function palin(str) {
+    if (str.length == 1){
+      return true;
+    } else if (str.length == 0){
+      return true;
+    } else if (str.length == 2 && str[0] == str[1]){
+      return true;
+    } else if (str.length == 2 && str[0] != str[1]){
+      return false;
+    } else if (str[0] == str[str.length - 1]) {
+      let newstr = '';
+      for (let index = 1;index < str.length - 1; index++){
+        newstr = newstr + str[index];
+      }
+      return palin(newstr);
+    } else {
+      return false;
+    }
+  }
+  return palin(str);
 }
